f108f462a3ef1fd8ea8b319d8fb0460b9b9143cc
gautamb97 <www.gautam971997@gmail.com>
2021-02-23 22:00:30 +0530

    Added:Handled null point exceptions

--- test/java/com/moodanalyser/MoodAnalyserTest.java
+++ test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -6,13 +6,19 @@ public class MoodAnalyserTest {
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad(){
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood();
+        String mood = moodAnalyser.analyseMood("Sad");
         Assert.assertEquals("SAD",mood);
     }
     @Test
     public void givenMessage_WhenNotSad_ShouldReturnHappy(){
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood();
+        String mood = moodAnalyser.analyseMood("Happy");
         Assert.assertEquals("HAPPY",mood);
+    }
+    @Test
+    public void givenNullMood_ShouldReturnHappy(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        String mood = moodAnalyser.analyseMood();
+        Assert.assertEquals("Happy",mood);
     }
 }

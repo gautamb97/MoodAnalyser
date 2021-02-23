87c4bb5d56ed951f170d742a916c6144eaffca2c
gautamb97 <www.gautam971997@gmail.com>
2021-02-23 22:32:59 +0530

    Added:Null mood throwing MoodAnalysisException

--- main/java/com/moodanalyser/MoodAnalyser.java
+++ main/java/com/moodanalyser/MoodAnalyser.java
@@ -7,18 +7,18 @@ public class MoodAnalyser {
     public MoodAnalyser(String message){
         this.message = message;
     }
-    public String analyseMood(String message){
+    public String analyseMood(String message) throws MoodAnalysisException {
         this.message = message;
         return analyseMood();
     }
-    public String analyseMood() {
+    public String analyseMood() throws MoodAnalysisException {
         try {
             if (message.contains("Sad"))
                 return "SAD";
             else
                 return "HAPPY";
         }catch(NullPointerException e){
-            return "Happy";
+            throw new MoodAnalysisException("Please enter proper message");
         }
     }
 }
--- test/java/com/moodanalyser/MoodAnalyserTest.java
+++ test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -1,24 +1,42 @@
 package com.moodanalyser;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class MoodAnalyserTest {
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad(){
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("Sad");
+        String mood = null;
+        try {
+            mood = moodAnalyser.analyseMood("Sad");
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
         Assert.assertEquals("SAD",mood);
     }
     @Test
     public void givenMessage_WhenNotSad_ShouldReturnHappy(){
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("Happy");
+        String mood = null;
+        try {
+            mood = moodAnalyser.analyseMood("Happy");
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
         Assert.assertEquals("HAPPY",mood);
     }
     @Test
     public void givenNullMood_ShouldReturnHappy(){
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-        String mood = moodAnalyser.analyseMood();
-        Assert.assertEquals("Happy",mood);
+        String mood = null;
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(MoodAnalysisException.class);
+            mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("Happy",mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
 }
